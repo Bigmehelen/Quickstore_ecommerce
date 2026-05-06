8b48239f5f6c9f46ef87fa6e4a2eb8988b2c9fbc
Helen <getfitbyhelena@gmail.com>
2026-05-06 09:23:29 +0100

    updated

--- QuickStore/urls.py
+++ QuickStore/urls.py
@@ -17,12 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 admin.site.site_header = "QuickStore Administration"
 admin.site.index_title = "Quick  Store Administration page"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("store.urls")),
+    path('', include('core.urls')),
 
     path("auth/",include('djoser.urls')),
 
